61008b725e178cb839db8b16aa98013ff579fcde
Songzm1999 <49391947+Songzm1999@users.noreply.github.com>
2020-05-12 12:53:06 +0800

    second user functional_test

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
-import unittest
 from selenium.webdriver.common.keys import Keys
 import time
 from django.test import LiveServerTestCase
+from selenium.common.exceptions import WebDriverException
+
+MAX_WAIT = 10
  
 class NewVisitorTest(LiveServerTestCase): #(1)
 	
@@ -14,12 +16,25 @@ class NewVisitorTest(LiveServerTestCase): #(1)
 	def tearDown(self):
 		self.browser.quit()
 
+	def wait_for_row_in_list_table(self,row_text):
+		start_time = time.time()
+		while True:
+			try:
+				table = self.browser.find_element_by_id('id_list_table')
+				rows = table.find_elements_by_tag_name('tr')
+				self.assertIn(row_text,[row.text for row in rows])
+				return
+			except (AssertionError,WebDriverException) as e:
+				if time.time() - start_time > MAX_WAIT:
+					raise e
+				time.sleep(0.5)
+
 	def check_for_row_in_list_table(self,row_text):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertIn(row_text,[row.text for row in rows])
 	
-	def test_can_start_a_list_and_retrive_it_later(self):
+	def test_can_start_a_list_for_one_user(self):
 		# Edith has heard about a cool new online to-do app. She goes 
 		# to check out its homepage
 		self.browser.get(self.live_server_url)
@@ -46,8 +61,7 @@ class NewVisitorTest(LiveServerTestCase): #(1)
 		# When she hits enter, the page updates,and now the page lists
 		# "1:Buy peacock feathers to make a fly" (Edith is very methodical)
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
-		self.check_for_row_in_list_table('1:Buy peacock feathers')
+		self.wait_for_row_in_list_table('1:Buy peacock feathers')
 
 		
 		#self.assertTrue(
@@ -60,11 +74,10 @@ class NewVisitorTest(LiveServerTestCase): #(1)
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
 
 		# The page updates again, and now shows both itmes on hre list
-		self.check_for_row_in_list_table('1:Buy peacock feathers')
-		self.check_for_row_in_list_table('2:Use peacock feathers to make a fly')
+		self.wait_for_row_in_list_table('1:Buy peacock feathers')
+		self.wait_for_row_in_list_table('2:Use peacock feathers to make a fly')
 
 		# Edith wonders whether the site will remember her lists ,Then shse sees
 		# that the site has generated a unique URL for her -- there is some
@@ -73,4 +86,50 @@ class NewVisitorTest(LiveServerTestCase): #(1)
 		# She visits that URL - her to -do list is still there
 
 		#Satisfied ,she goes back to sleep
-		self.fail('Finish the test!')	#self.fail无论如何都会失败，产生错误信息。 我们用它作为完成测试的提醒。
+		# self.fail('Finish the test!')	#self.fail无论如何都会失败，产生错误信息。 我们用它作为完成测试的提醒。
+
+	
+	def test_multiple_users_can_start_lists_at_different_urls(self):
+        # 乔伊新建一个待办事项清单
+		self.browser.get(self.live_server_url)
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy peacock feathers')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1:Buy peacock feathers')
+
+		# 她注意到清单有个唯一的URL
+		edith_list_url = self.browser.current_url
+		self.assertRegex(edith_list_url, '/lists/.+')
+
+		# 现在一名焦作弗朗西斯的新用户访问了网站
+
+		## 我们使用了一个新浏览器会话
+		## 确保乔伊的信息不会从cookie中泄露出去
+		self.browser.quit()
+		self.browser = webdriver.Firefox()
+
+		# 弗朗西斯访问首页
+		# 页面中看不到乔伊的清单
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertNotIn('make a fly', page_text)
+
+		# 弗朗西斯输入一个新待办事项，新建一个清单
+		# 他不像乔伊那样兴趣盎然
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy milk')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1:Buy milk')
+
+		# 弗朗西斯获得了他的唯一URL
+		francis_list_url = self.browser.current_url
+		self.assertRegex(francis_list_url, '/lists/.+')
+		self.assertNotEqual(francis_list_url, edith_list_url)
+
+		# 这个页面还是没有乔伊的清单
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertIn('Buy milk', page_text)
+
+		#两个人都很满意，然后去休息了
