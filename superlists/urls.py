0b135cd6b95651dfa38a8153cf26cb8b14a8b2d8
Songzm1999 <49391947+Songzm1999@users.noreply.github.com>
2020-06-02 19:23:49 +0800

    change superlists/urls

--- superlists/urls.py
+++ superlists/urls.py
@@ -19,7 +19,7 @@ from lists import views
 from lists import urls as list_urls
  
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    # url(r'^admin/', admin.site.urls),
     url(r'^$', views.home_page, name='home'),
     url(r'^lists/', include(list_urls)),
 ]
